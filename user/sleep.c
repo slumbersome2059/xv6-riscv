44b0526c898afed577bf6a9b9f143c8d26c6cec1
Annamalai <an.nachiappan06@gmail.com>
2026-07-07 22:07:53 +0100

    Created sleep command

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        fprintf(2, "No arguments provided\n");
+    }else{
+        int sleepTime = 0;
+        for(int i = 1; i < argc;i++){//look at how this starts from 1, first arg is sleep
+            sleepTime += atoi(argv[i]);
+        }
+        if(pause(sleepTime)!= 0){
+            fprintf(2, "Error in pause sys-call");
+        }
+    }
+    return 0;
+}
